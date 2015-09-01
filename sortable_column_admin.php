0f60ecf235948d37d657db58e2db31f2e6cfe6f3
ikalangita <ikalangitahaja@gmail.com>
2015-09-01 11:31:43 +0200

    test commit

--- sortable_column_admin.php
+++ sortable_column_admin.php
@@ -2,7 +2,7 @@
 
 	/*
 	============================================
-		SECTEURS
+		COLONNES SECTEURS
 	============================================
 	*/
 	add_filter('manage_edit-offre_emploi_columns', 'offre_emploi_column_register');
@@ -10,9 +10,11 @@
 	add_action('manage_offre_emploi_posts_custom_column', 'offre_emploi_column_display', 10, 2);
 
 	function offre_emploi_column_register( $columns ) {
+
+		$columns['secteur'] = __( 'secteur', 'kblt' );
     	$columns['contrat'] = __( 'contrat', 'kblt' );
     	$columns['lieu'] 	= __( 'lieu', 'kblt' );
-    	$columns['secteur'] = __( 'secteur', 'kblt' );
+    	
 	    return $columns;
 	}
 	
@@ -21,19 +23,8 @@
 		global $post;
 		global $wpdb;
 
-	    if ( 'contrat' == $column_name ){
-
-	        $contrat = get_post_meta($post->ID, 'contrat', true);
-	        echo $contrat;
-	    }
-
-	    if ( 'lieu' == $column_name ){
-
-	        $lieu = get_post_meta($post->ID, 'lieu', true);
-	        echo $lieu;
-	    }
-
-	    if ( 'secteur' == $column_name ){
+		# Afficher la colonne secteur
+		if ( 'secteur' == $column_name ){
 
 	        $secteur 	= get_post_meta($post->ID, 'field_id', true);
 	       	$exp 	 	= explode("_", $secteur);
@@ -49,6 +40,20 @@
 	       		echo $s["libel_secteur"];
 	       	}
 	    }
+
+	    if ( 'contrat' == $column_name ){
+
+	        $contrat = get_post_meta($post->ID, 'contrat', true);
+	        echo $contrat;
+	    }
+
+	    if ( 'lieu' == $column_name ){
+
+	        $lieu = get_post_meta($post->ID, 'lieu', true);
+	        echo $lieu;
+	    }
+
+	    
 	}
 
 	function sort_columns( $vars ) {
@@ -72,4 +77,3 @@
 		return $vars;
 	}
 	add_filter('request', 'sort_columns');
-	
