0ea30edbce737fd4c360476971e9308834b4a8df
ikalangita <ikalangitahaja@gmail.com>
2015-09-01 11:36:57 +0200

    Modification titre

--- sortable_column_admin.php
+++ sortable_column_admin.php
@@ -2,7 +2,7 @@
 
 	/*
 	============================================
-		COLONNES SECTEURS
+		WP COLONNES SECTEURS
 	============================================
 	*/
 	add_filter('manage_edit-offre_emploi_columns', 'offre_emploi_column_register');
